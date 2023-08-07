20ea2a340e94ac68903e8b19def474c0a0947dc8
Francisco Rodrigues <126619954+FrancW2023@users.noreply.github.com>
2023-08-07 20:06:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+//Bom Dia!
+img.src = "img/manhã.jpg"
+
+} else if (hora >= 12 &&  hora <= 18) {
+    //Boa tarde!
+    img.src = "img/tarde.jpg"
+} else {
+    //Boa noite!
+    img.src = "img/noite.jpg" 
+}
+
+
+}
